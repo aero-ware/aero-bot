3b835ac6b1540244200df9964495b757c389a794
Dheeraj Prakash <dheerajpv16@gmail.com>
2020-12-23 19:41:30 -0500

    no unnecessary pings in ping cmd

--- commands/misc/ping.js
+++ commands/misc/ping.js
@@ -3,7 +3,7 @@ module.exports = {
     description: 'Tells you the Discord API latency and also the latency of this bot',
     category: 'Misc',
     run: (message, args, text, client) => {
-        message.reply('calculating ping...').then(sentMessage => {
+        message.channel.send('Calculating ping...').then(sentMessage => {
             const ping = sentMessage.createdTimestamp - message.createdTimestamp
             sentMessage.edit(`**Bot latency:** ${ping}ms, **Discord API latency:** ${client.ws.ping}ms`)
         })
