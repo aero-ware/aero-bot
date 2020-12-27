8e71c6c6fbdcaae4a0d03f8216907ea6ec02208a
Dheeraj Prakash <dheerajpv16@gmail.com>
2020-12-27 15:21:32 -0500

    deleting all the things when kicked from guild

--- features/guild-join-leave.js
+++ features/guild-join-leave.js
@@ -1,4 +1,7 @@
 const guildSchema = require("../schemas/guild-schema")
+const muteSchema = require('../schemas/mute-schema')
+const tempbanSchema = require('../schemas/tempban-schema')
+const memberSchema = require('../schemas/member-schema')
 
 module.exports = async client => {
     client.on('guildCreate', async guild => {
@@ -16,5 +19,9 @@ module.exports = async client => {
     client.on('guildDelete', async guild => {
         // delete the db entry if it got kicked from a guild.
         await guildSchema.findOneAndDelete({ _id: guild.id })
+        // also delete all the economy, mutes, tempbans etc from the db
+        await muteSchema.deleteMany({ guildId: guild.id })
+        await tempbanSchema.deleteMany({ guildId: guild.id })
+        await memberSchema.deleteMany({ guildId: guild.id })
     })
 }
