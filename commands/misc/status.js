30e8f256d733dc0c5ff1df26b143897f7ac7c4bb
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-01-02 19:19:22 -0500

    owner only status command

---
+++ commands/misc/status.js
@@ -0,0 +1,43 @@
+module.exports = {
+    commands: ['status', 'setstatus'],
+    category: 'Misc',
+    description: 'owner only command. Sets the bot\'s status.',
+    ownerOnly: true,
+    expectedArgs: '<text>',
+    run: ({ message, client, args }) => {
+        let [type, ...activity] = args
+        activity = activity.join(' ')
+
+        switch (type.toLowerCase()) {
+            case 'playing':
+                type = 'PLAYING'
+                break
+            
+            case 'streaming':
+                type = 'STREAMING'
+                break
+
+            case 'listening':
+                type = 'LISTENING'
+                break
+
+            case 'watching':
+                type = 'WATCHING'
+                break
+
+            case 'competing':
+                type = 'COMPETING'
+                break
+
+            default:
+                return message.reply('invalid prescence type.')
+        }
+
+        client.user.setPresence({
+            activity: {
+                name: activity,
+                type
+            }
+        })
+    }
+}
