68f24c74537f38e333ce242293cda2737ab06dc4
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-01-02 19:55:26 -0500

    fixed avatar

--- commands/fun/avatar.js
+++ commands/fun/avatar.js
@@ -8,7 +8,13 @@ module.exports = {
     run: async ({ message, text, client }) => {
         let target = message.author
         if (message.mentions.users.first()) target = message.mentions.users.first()
-        else if (text && await client.users.fetch(text)) target = await client.users.fetch(text)
+        else if (text) {
+            let invalidID = false
+            const user = await client.users.fetch(text).catch(() => invalidID = true)
+            if (!invalidID) target = user
+            else return message.reply('invalid user ID.')            
+        }
+        // else if (text && await client.users.fetch(text)) target = await client.users.fetch(text)
 
         return message.channel.send(target.displayAvatarURL({ dynamic: true, size: 512 }))
     }
