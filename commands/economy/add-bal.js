0dad9be3dd67515e5c52e1593b38d615373339ff
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-01-06 15:33:30 -0500

    using messages.json more

--- commands/economy/add-bal.js
+++ commands/economy/add-bal.js
@@ -1,21 +1,20 @@
+const { GuildMember } = require("discord.js")
 const economy = require("../../util/economy")
+const getTarget = require("../../util/get-target")
 
 module.exports = {
     commands: ['addbalance', 'addbal'],
     minArgs: 1,
     maxArgs: 2,
     ownerOnly: true,
-    expectedArgs:'[targer user ping] <amount>',
+    expectedArgs:'<targer user ping|id> <amount>',
     description: 'adds money to the target user\'s bank',
     category: 'Economy',
-    callback: async ({ message, args }) => {
-        let target = message.mentions.users.first()
-        let noTarget = false
-        if (!target) {
-            noTarget = true
-            target = message.author
-        }
-        const coins = noTarget ? args[0] : args[1]
+    callback: async ({ message, args, instance }) => {
+        const target = await getTarget.firstArgPingID(message, args, instance)
+        if (!target) return
+
+        const coins = args[1]
         if (isNaN(coins)) return message.reply('Please provide a valid number of coins.')
         const newCoins = await economy.addCoins(message.guild.id, target.id, coins)
         message.reply(`You have given ${target} ${coins} ${coins == 1 ? 'coin' : 'coins'}. They now have ${newCoins} ${newCoins == 1 ? 'coin' : 'coins'}.`)
