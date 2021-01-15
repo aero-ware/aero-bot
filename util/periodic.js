8e5433f5e2bbdc01dee52c014f460280db1a1029
Dheeraj Prakash <dheerajpv16@gmail.com>
2021-01-15 14:45:35 -0500

    ability to give bot custom status and clear it

--- util/periodic.js
+++ util/periodic.js
@@ -4,7 +4,7 @@ const updatetempBans = require("./tempban-checker")
 module.exports = (client) => {
     updateMutes(client)
     updatetempBans(client)
-    client.user.setPresence({
+    if (client.user.presence.activities.length === 0 || /\d+ \w+/g.test(client.user.presence.activities[0].name)) client.user.setPresence({
         activity: {
             type: 'WATCHING',
             name: `${client.guilds.cache.size} Servers! >help`
